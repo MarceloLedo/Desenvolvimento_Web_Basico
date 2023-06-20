a06274a6885a8fb540b91ced524a99b9a894a115
MarceloLedo <113719068+MarceloLedo@users.noreply.github.com>
2023-06-20 18:54:58 -0300

    Commit do trabalho integrador

---
+++ Integrador 2ºSemestre/api/src/routes.js
@@ -0,0 +1,27 @@
+import {Router} from 'express'
+import ChamadoControllers from './app/controllers/ChamadoControllers.js'
+import SetorControllers from './app/controllers/SetorControllers.js'
+import CadastroControllers from './app/controllers/CadastroControllers.js'
+
+const router = Router()
+
+//ROTAs dos chamados
+router.get('/chamado', ChamadoControllers.index)
+router.get('/chamado/:id', ChamadoControllers.show)
+router.post('/chamado', ChamadoControllers.store)
+router.put('/chamado/:id', ChamadoControllers.update)
+router.delete('/chamado/:id', ChamadoControllers.delete)
+//ROTAS dos setores
+router.get('/setor', SetorControllers.index)
+router.get('/setor/:id', SetorControllers.show)
+router.post('/setor', SetorControllers.store)
+router.put('/setor/:id', SetorControllers.update)
+router.delete('/setor/:id', SetorControllers.delete)
+//ROTAS dos Cadastros
+router.get('/cadastro', CadastroControllers.index)
+router.get('/cadastro/:id', CadastroControllers.show)
+router.post('/cadastro', CadastroControllers.store)
+router.put('/cadastro/:id', CadastroControllers.update)
+router.delete('/cadastro/:id', CadastroControllers.delete)
+
+export default router
